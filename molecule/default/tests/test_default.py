7398c2bedc481402960f1a300b25135e6dc08514
darkwizard242 <am900820@gmail.com>
2019-10-19 23:45:40 -0400

    enforced assertion for testinfra

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,33 +6,27 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-CHROME_BINARY_PATH = '/usr/bin/google-chrome'
-CHROME_PACKAGE = 'google-chrome-stable'
-CHROME_DEBIAN_REPO = '/etc/apt/sources.list.d/google-chrome.list'
-CHROME_EL_REPO = '/etc/yum.repos.d/google-chrome.repo'
-
-
 def test_googlechrome_package_installed(host):
-    host.package("CHROME_PACKAGE").is_installed
+    assert host.package("google-chrome-stable").is_installed
 
 
 def test_googlechrome_binary_exists(host):
-    host.file('CHROME_BINARY_PATH').exists
+    assert host.file('/usr/bin/google-chrome').exists
 
 
 def test_googlechrome_binary_symlink(host):
-    host.file('CHROME_BINARY_PATH').is_symlink
+    assert host.file('/usr/bin/google-chrome').is_symlink
 
 
 def test_googlechrome_binary_which(host):
-    host.check_output('which google-chrome') == CHROME_BINARY_PATH
+    assert host.check_output('which google-chrome') == '/usr/bin/google-chrome'
 
 
 def test_atom_repo_exists(host):
-    host.file('CHROME_DEBIAN_REPO').exists or \
-      host.file('CHROME_EL_REPO').exists
+    assert host.file('/etc/apt/sources.list.d/google-chrome.list').exists or \
+      host.file('/etc/yum.repos.d/google-chrome.repo').exists
 
 
 def test_atom_repo_file(host):
-    host.file('CHROME_DEBIAN_REPO').is_file or \
-      host.file('CHROME_EL_REPO').is_file
+    assert host.file('/etc/apt/sources.list.d/google-chrome.list').is_file or \
+      host.file('/etc/yum.repos.d/google-chrome.repo').is_file
